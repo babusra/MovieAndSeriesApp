8ab8d535c915e77e55a2579256462ab432759109
Büşra <busrarigun@gmail.com>
2021-11-15 21:49:41 +0300

    Navigation added

---
+++ src/screens/ListScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, StyleSheet, View} from "react-native";
+
+const ListScreen=()=>{
+    return(
+    <View>
+        <Text>ListScreen</Text>
+    </View>
+    )};
+
+const styles=StyleSheet.create({});
+
+export default ListScreen;
