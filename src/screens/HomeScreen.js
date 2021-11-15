ae88282399ca5bd8252c4b898e68d10be102db37
Büşra <busrarigun@gmail.com>
2021-11-15 20:57:38 +0300

    Initial commit

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text,StyleSheet,View } from "react-native";
+
+const HomeScreen=()=>{
+  return(
+  <View>
+    <Text>Home Page</Text>
+  </View>
+  )};
+
+const styles=StyleSheet.create({});
+
+export default HomeScreen;
