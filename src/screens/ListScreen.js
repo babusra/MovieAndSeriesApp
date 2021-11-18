9ea3a1b47b3dc6ae01a180ca6259f18681510c2c
Büşra <busrarigun@gmail.com>
2021-11-18 18:00:07 +0300

    searchbar added

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import { Text, StyleSheet, View} from "react-native";
+import { Searchbar,Provider as PaperProvider, Button } from "react-native-paper";
 
 const ListScreen=()=>{
+
+  const [searchQuery,setSearchQuery]=useState("");
+  const onChangeSearch = query => setSearchQuery(query);
+
     return( 
+    <PaperProvider>
     <View>
-       <Text>ListScreen</Text>
+    <Searchbar style={styles.SearchStyle} placeholder="Film / Dizi / Oyuncu ara" onChangeText={onChangeSearch} value={searchQuery}/>
     </View>
+    </PaperProvider>
+ 
   )};
 
-const styles=StyleSheet.create({});
+const styles=StyleSheet.create({
+  SearchStyle:{
+    width:290
+  } 
+});
 
 export default ListScreen;
