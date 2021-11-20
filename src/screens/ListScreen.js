4f642d87a35466d1cd7a00a768641545cedf35bd
Büşra <busrarigun@gmail.com>
2021-11-20 23:50:41 +0300

    searchbar updated and dropdown added

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,23 +1,99 @@
-
 import React, {useState} from "react";
-import { Text, StyleSheet, View} from "react-native";
+import { Text, StyleSheet,View} from "react-native";
+import { FlatList } from "react-native-gesture-handler";
 import { Searchbar} from "react-native-paper";
+import DropDownPicker from "react-native-dropdown-picker";
+const serMovData = require('../data/seriesMoviesdata.json').entries;
 
-const ListScreen=()=>{
+
+const ListScreen=(props)=>{
 
   const [searchQuery,setSearchQuery]=useState("");
-  const onChangeSearch = query => setSearchQuery(query);
+
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState('');
+  const [items, setItems] = useState([
+    {label: 'Yeniye Göre Sırala', value: '0'},
+    {label: 'Eskiye Göre Sırala', value: '1'},
+    {label: 'Puana Göre Sırala', value: '2'},
+    {label: 'Rastgele Sırala', value: '3'},
+
+  ]);
+
+  const programType=props.navigation.state.params.programType;
+  
+  const[pType,setPType]=useState(programType);
+
+  const filteredData=()=>{
+    var r=serMovData.filter(function(item){
+      return item.programType==pType;
+    });
+
+    if (searchQuery.length>2){
+      r=r.filter(function(item){
+        return item.title.includes(searchQuery);
+      });
+    }
+    r.sort(function(a, b) {
+      switch(value){
+
+          case '0':
+              return a.releaseYear<b.releaseYear?1:-1;
+          case '1':
+              return a.releaseYear>b.releaseYear?1:-1;
+          case '3':
+              return Math.round(Math.random());
+      }
+
+    });
+
+    return r;
+}
+
+
+  const data=filteredData();
+  
 
     return( 
-    <View>
-    <Searchbar style={styles.SearchStyle} placeholder="Film / Dizi / Oyuncu ara" onChangeText={onChangeSearch} value={searchQuery}/>
+    <View style={styles.container}>
+
+    <Searchbar 
+        placeholder="Film / Dizi / Oyuncu ara"
+        onChangeText={(value)=>{setSearchQuery(value);}}
+        value={searchQuery}/>
+
+
+    <FlatList 
+        data={data} 
+        renderItem={({item})=>{return <Text>{item.title}</Text>;}}></FlatList>
+  
+
+
+  
+    <DropDownPicker
+      open={open}
+      value={value}
+      items={items}
+      setOpen={setOpen}
+      setValue={setValue}
+      setItems={setItems}
+      placeholder="Sırala"
+    />
     </View>
+
+
+
  
-  )};
+  );};
 
 const styles=StyleSheet.create({
   SearchStyle:{
-    width:290
+    width:290,
+    marginTop:20,
+    marginBottom:10
+  },
+  container:{
+    marginBottom:200
   } 
 });
 
